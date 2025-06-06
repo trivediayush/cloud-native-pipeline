adceeb62497b2bc6c75a8b3805afa18d553000e2
Ayush Trivedi <ayushtrivedi890@gmail.com>
2025-06-06 20:59:52 +0530

    Pipeline Created

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -2,6 +2,6 @@ package com.example.app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, CI/CD Pipeline with Jenkins and SonarQube!");
+        System.out.println("Hello, CI/CD Pipeline with Jenkins, Java, Maven and SonarQube!");
     }
 }
